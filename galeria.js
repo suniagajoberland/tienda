65d338866f49c1632bdf7ce9f91d5220e86d88c1
joberland <suniagajoberland@gmail.com>
2024-11-28 19:43:06 -0400

    ficha

--- galeria.js
+++ galeria.js
@@ -4,17 +4,34 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Datos de productos ingresados manualmente con imágenes referenciadas desde la carpeta 'images'
   const productos = [
-    {
+    /* {
       nombre: "Watch Onn Google TV 4k",
       descripcion: "https://example.com/ficha-tecnica-producto-1",
       costo: 35,
       imagen: "Assets/Watchonn4k.jpg", // Ruta de la imagen en la carpeta 'images'
-    },
+    }, */
     {
       nombre: "Watch Onn Google TV 2k",
-      descripcion: "https://example.com/ficha-tecnica-producto-2",
+      descripcion:
+        "https://www.amazon.com/-/es/Streaming-adaptador-corriente-Chromecast-incorporado/dp/B0B61V76X1",
       costo: 30,
       imagen: "Assets/Watchonn2k.jpg", // Ruta de la imagen en la carpeta 'images'
+    },
+    {
+      nombre: "Wireplus - Mini UPS 21w 2200Mahx4",
+      descripcion:
+        "https://www.grupo-plus.com/shop/wp-mini-21w-mini-dc-ups-21w-2200mahx4-120v-ac-input-output-usb-5v-dc-9v-o-12v-poe-15v-24v-30045#attr=6014",
+      costo: 35,
+      imagen:
+        "Assets/Wireplus - Mini UPS 21w 2200Mahx4 120v-ac  input output USB 5v Dc 9V o 12v poe 15V-24V.jpg", // Ruta de la imagen en la carpeta 'images'
+    },
+    {
+      nombre: "Wireplus - Mini UPS 21w 2200Mahx4",
+      descripcion:
+        "https://www.grupo-plus.com/shop/wp-mini-21w-mini-dc-ups-21w-2200mahx4-120v-ac-input-output-usb-5v-dc-9v-o-12v-poe-15v-24v-30045#attr=6014",
+      costo: 35,
+      imagen:
+        "Assets/Wireplus - Mini UPS 21w 2200Mahx4 120v-ac  input output USB 5v Dc 9V o 12v poe 15V-24V.jpg", // Ruta de la imagen en la carpeta 'images'
     },
     // Añade más productos según sea necesario
   ];
