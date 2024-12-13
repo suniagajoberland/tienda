79f72ea88e2ce9634848505e0fee810ffc573dc5
joberland <suniagajoberland@gmail.com>
2024-12-12 22:33:32 -0400

    aire

--- galeria.js
+++ galeria.js
@@ -237,7 +237,8 @@ document.addEventListener("DOMContentLoaded", () => {
     },
     {
       nombre: "Cable Utp Panduit Cat6 100% Cob Ind 305m Azul",
-      descripcion: "https://www.panduit.com/content/dam/panduit/en/products/media/7/17/417/6417/90376417.pdf",
+      descripcion:
+        "https://www.panduit.com/content/dam/panduit/en/products/media/7/17/417/6417/90376417.pdf",
       costo: 200,
       imagen: "Assets/panduit.webp", // Ruta de la imagen en la carpeta 'images'
     },
@@ -283,12 +284,26 @@ document.addEventListener("DOMContentLoaded", () => {
       costo: 17,
       imagen: "Assets/aa220.jpg", // Ruta de la imagen en la carpeta 'images'
     },
-/*     {
+    {
+      nombre: "Protector De Voltaje A/acond. Y Refrigeración 110v 30amp",
+      descripcion:
+        "https://lumistar.com.ve/productos/ptv003-protector-de-voltaje/",
+      costo: 22.3,
+      imagen: "Assets/aabornera.jpg", // Ruta de la imagen en la carpeta 'images'
+    },
+    {
+      nombre: "Protector De Voltaje A/acond. Y Refrigeracion 220v 30amp",
+      descripcion:
+        "https://lumistar.com.ve/productos/ptv004-protector-de-voltaje/",
+      costo: 22.3,
+      imagen: "Assets/aabonera220.jpg", // Ruta de la imagen en la carpeta 'images'
+    },
+    /*     {
       nombre: "",
       descripcion: "",
       costo: 200,
       imagen: "", // Ruta de la imagen en la carpeta 'images'
-    }, */
+    },  */
     // Añade más productos según sea necesario
   ];
 
